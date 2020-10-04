373243a6c0f1e2d1c59a9c40432a49bbb171936d
moliu9 <mo_liu@brown.edu>
2020-10-04 18:34:54 -0400

    change

--- test_song_analysis.py
+++ test_song_analysis.py
@@ -23,17 +23,46 @@ def testing_compute_idf():
 
 
 def testing_compute_tf():
-    pass
+
+    # the empty list
+    assert compute_tf([]) == {}
+
+    # the empty string
+    assert compute_tf(['']) == {'': 1}
+
+    # non-empty entries
+    lyrics = clean_lyrics('happy birthday to you')
+    assert compute_tf(lyrics) == {'happy': 1, 'birthday': 1, 'to': 1, 'you': 1}
+
+    # non-empty, with numbers
+    lyrics = clean_lyrics('h8ppy birthd3y to y0u')
+    assert compute_tf(lyrics) == {'h8ppy': 1, 'birthd3y': 1, 'to': 1, 'y0u': 1}
+
+    # non-empty, with bad characters
+    lyrics = clean_lyrics('t[]his w^\old i,s on\ fir[e')
+    assert compute_tf(lyrics) == {'this': 1, 'wold': 1, 'is': 1, 'on': 1, 'fire': 1}
 
 
 def testing_compute_tf_idf():
-    pass
+
+    # empty word list and empty corpus
+    assert compute_tf_idf([], {}) == {}
+
+    # only unique words in lyrics
+    idf = compute_idf(create_corpus('example.csv'))
+    lyrics = clean_lyrics('close to love')
+    assert compute_tf_idf(lyrics, idf) == {'close': math.log(7), 'to': math.log(7/3), 'love': math.log(7/2)}
+
+    # repeating words in lyrics
+    lyrics = clean_lyrics('this is now this is love love love')
+    assert compute_tf_idf(lyrics, idf) == {'this': 2*math.log(7), 'is': 2*math.log(7),
+                                           'now': math.log(7/2), 'love': 3*math.log(7/2)}
 
 
 def testing_corpus_tf_idf():
     pass
 
 
-def testing_nearest_neightbor():
+def testing_nearest_neighbor():
     pass
 
