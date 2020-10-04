4447cc9c75a25730da89aa903258b402c18af879
moliu9 <mo_liu@brown.edu>
2020-10-04 15:04:13 -0400

    change

--- test_song_analysis.py
+++ test_song_analysis.py
@@ -1,5 +1,6 @@
 from song_analysis import *
 import pytest
+import math
 
 
 # testing compute_idf
@@ -10,21 +11,16 @@ def testing_compute_idf():
 
     # two entries with no overlapping words: all idf values should be equal
     short_unique = create_corpus('short_unique.csv')
-    assert compute_idf(short_unique) == {'love': 0.6931, 'lyrics': 0.6931, 'baby': 0.6931, 'i': 0.6931,
-                                         'birthday': 0.6931, 'you': 0.6931, 'happy': 0.6931}
+    assert compute_idf(short_unique) == {'love': math.log(2), 'lyrics': math.log(2),
+                                         'baby': math.log(2), 'i': math.log(2),
+                                         'birthday': math.log(2), 'you': math.log(2), 'happy': math.log(2)}
 
     # two entries with overlapping words: overlapped should have idf value of zero
-    # short_overlap = create_corpus('short_overlap.csv')
-    # assert compute_idf(short_overlap) == {'': 0.6931,
-    #                                       'to': 0.6931471805599453,
-    #                                       'lyrics': 0.6931471805599453,
-    #                                       'love': 0.6931471805599453,
-    #                                       'i': 0.6931471805599453,
-    #                                       'birthday': 0.6931471805599453,
-    #                                       'you': 0.0,
-    #                                       'happy': 0.6931471805599453}
+    short_overlap = create_corpus('short_overlap.csv')
+    assert compute_idf(short_overlap) == {'': math.log(2), 'to': math.log(2), 'lyrics': math.log(2),
+                                          'love': math.log(2), 'i': math.log(2),
+                                          'birthday': math.log(2), 'you': math.log(1), 'happy': math.log(2)}
 
-    # three entries with same words appearing in multiple songs
 
 def testing_compute_tf():
     pass
